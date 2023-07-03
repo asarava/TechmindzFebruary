333b431acc62403ebc8580c4d92c4a3c4a26bbd2
Jintupgeorge <137852604+Jintupgeorge@users.noreply.github.com>
2023-07-03 14:50:27 +0530

    submit

--- ProjectWorkspace/SeleniumProjectV1/src/main/java/com/testbase/TestBase.java
+++ ProjectWorkspace/SeleniumProjectV1/src/main/java/com/testbase/TestBase.java
@@ -1,5 +1,51 @@
 package com.testbase;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class TestBase {
 
+	public static WebDriver driver;
+	public static Properties configProp;
+
+	public TestBase() {
+		// Reading Config File.
+		try {
+			configProp = new Properties();
+			FileInputStream readConfigLoc = new FileInputStream(
+					"C:\\Users\\Butterfly\\Desktop\\Repository Project\\TechmindzFebruary\\ProjectWorkspace\\SeleniumProjectV1\\src\\main\\java\\com\\config\\Config");
+			configProp.load(readConfigLoc);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	
+
+	// Initializing web browser
+	@SuppressWarnings("deprecation")
+	public static void initialization() {
+		String browserName = configProp.getProperty("browser");
+
+		if (browserName.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		}
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.get(configProp.getProperty("URL"));
+
+	}
 }
